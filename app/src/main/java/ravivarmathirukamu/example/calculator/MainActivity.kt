7158b7bd874d642779a68414487a3e2b96a91329
Thirukamu, Ravivarma <ravivarma.thirukamu@verizonwireless.com>
2019-05-07 19:00:50 -0400

    adding on click function.

--- app/src/main/java/ravivarmathirukamu/example/calculator/MainActivity.kt
+++ app/src/main/java/ravivarmathirukamu/example/calculator/MainActivity.kt
@@ -3,9 +3,16 @@ package ravivarmathirukamu.example.calculator
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var addNumber:String? = ""
+    var numberOne:Float? = null
+    var numberTwo:Float? = null
+    var result:String? = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,9 +20,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun operationFunction(view:View) {
+
+        var editText: EditText = findViewById(R.id.show_number)
+
         
         when(view.id) {
-            R.id.button_one -> Toast.makeText(this,"Button 1 works",Toast.LENGTH_SHORT).show()
+
+            R.id.button_one -> {
+                addNumber += "1"
+                editText.setText(addNumber)
+            }
+
+            R.id.reset_button -> {
+                addNumber = ""
+                editText.setText("")
+            }
+
+            R.id.add_button -> {
+               
+            }
+
         }
     }
 }
